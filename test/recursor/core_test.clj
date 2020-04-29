69fc7aa2e4ff808fe40e306dd8648df975db2964
Divyansh Prakash <divyansh.prakash@yahoo.com>
2020-04-29 16:54:46 +0530

    Refactor tests

--- test/recursor/core_test.clj
+++ test/recursor/core_test.clj
@@ -2,8 +2,8 @@
   (:require [clojure.test :refer [deftest is]]
             [recursor.core :as r :refer [defrec recurse return]]))
 
-(defrec ^{::r/cache-size 5000}
-  ackermann
+(defrec ^{::r/cache-size 100000}
+  A
   "Calculates the ackermann function:
     A(0, n) = n+1
     A(m, 0) = A(m-1, 1)
@@ -14,15 +14,14 @@
     (return (inc n))
 
     (zero? n)
-    (recurse (ackermann (dec m) 1))
+    (recurse (A (dec m) 1))
 
     :else
     (recurse
-     (ackermann m (dec n))
-     :then #(ackermann (dec m) %))))
+     (A m (dec n))
+     :then #(A (dec m) %))))
 
-(deftest off-stack-recursion-test []
-  (is (.startsWith (-> #'ackermann meta :doc)
-                   "Calculates the ackermann"))
-  (is (== 5000 (-> #'ackermann meta ::r/cache-size)))
-  (is (== 8189 (ackermann 3 10))))
+(deftest recursor-test []
+  (is (-> #'A meta :doc (.startsWith "Calculates the ackermann")))
+  (is (== 100000 (-> #'A meta ::r/cache-size)))
+  (is (== 8189 (A 3 10))))
