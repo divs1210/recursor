ef9fe195d3a8a4cb3d8257426223478ea8f732e3
Divyansh Prakash <divyansh.prakash@yahoo.com>
2020-05-23 17:48:58 +0530

    Increase test coverage

--- test/recursor/core_test.clj
+++ test/recursor/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer [deftest is]]
             [recursor.core :as r :refer [defrec recurse return]]))
 
+(defrec fact [n]
+  (if (zero? n)
+    (return 1)
+    (recurse
+     (fact (dec n))
+     :then #(*' n %))))
+
+(deftest recursor-test
+  (is (== 120 (fact 5)))
+  (is (== (apply *' (range 1 5001))
+          (fact 5000))))
+
+
 (defrec ^{::r/cache-size 100000}
   A
   "Calculates the ackermann function:
@@ -21,7 +34,7 @@
      (A m (dec n))
      :then #(A (dec m) %))))
 
-(deftest recursor-test []
+(deftest caching-test
   (is (-> #'A meta :doc (.startsWith "Calculates the ackermann")))
   (is (== 100000 (-> #'A meta ::r/cache-size)))
   (is (== 8189 (A 3 10))))
